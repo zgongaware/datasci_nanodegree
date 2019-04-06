5fcfa7f9006fbc47e339112128982402aa119f88
Zach Gongaware <zgongaware@gmail.com>
2019-04-06 11:28:33 -0700

    '2.8'

--- misc/scratch_sheet.py
+++ misc/scratch_sheet.py
@@ -1,5 +1,5 @@
 
-# Quiz 2.7.8
+# Quiz 2.7.8 - ADABoost Weight
 from math import log
 
 
@@ -10,3 +10,11 @@ def calculate_adaboost_weight(accuracy):
 m1 = calculate_adaboost_weight(7/8)
 m2 = calculate_adaboost_weight(4/8)
 m3 = calculate_adaboost_weight(2/8)
+
+# Quiz 2.8.13 - F1 Score
+
+
+def calculate_f1_score(precision, recall):
+    return (2 * precision * recall) / (precision + recall)
+
+a = calculate_f1_score(0.556, 0.833)
