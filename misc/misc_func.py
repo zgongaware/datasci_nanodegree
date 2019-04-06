3644adb80d1c5d2e55299c466fc1aa64c4723dc0
Zach Gongaware <zgongaware@gmail.com>
2019-04-06 11:29:35 -0700

    'misc_func'

--- misc/misc_func.py
+++ misc/misc_func.py
@@ -27,3 +27,84 @@ def plot_model(X, y, clf):
     if len(np.unique(z)) > 1:
         plt.contour(s,t,z,colors = 'k', linewidths = 2)
     plt.show()
+
+
+def build_roc_auc(model, X_train, X_test, y_train, y_test):
+    '''
+    INPUT:
+    model - an sklearn instantiated model
+    X_train - the training data
+    y_train - the training response values (must be categorical)
+    X_test - the test data
+    y_test - the test response values (must be categorical)
+    OUTPUT:
+    auc - returns auc as a float
+    prints the roc curve
+    '''
+    import numpy as np
+    import matplotlib.pyplot as plt
+    from itertools import cycle
+    from sklearn.metrics import roc_curve, auc, roc_auc_score
+    from scipy import interp
+
+    y_preds = model.fit(X_train, y_train).predict_proba(X_test)
+    # Compute ROC curve and ROC area for each class
+    fpr = dict()
+    tpr = dict()
+    roc_auc = dict()
+    for i in range(len(y_test)):
+        fpr[i], tpr[i], _ = roc_curve(y_test, y_preds[:, 1])
+        roc_auc[i] = auc(fpr[i], tpr[i])
+
+    # Compute micro-average ROC curve and ROC area
+    fpr["micro"], tpr["micro"], _ = roc_curve(y_test.ravel(), y_preds[:, 1].ravel())
+    roc_auc["micro"] = auc(fpr["micro"], tpr["micro"])
+
+    plt.plot(fpr[2], tpr[2], color='darkorange',
+             lw=2, label='ROC curve (area = %0.2f)' % roc_auc[2])
+    plt.plot([0, 1], [0, 1], color='navy', lw=2, linestyle='--')
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.05])
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title('Receiver operating characteristic example')
+    plt.show()
+
+    return roc_auc_score(y_test, np.round(y_preds[:, 1]))
+
+
+def r2(actual, preds):
+    '''
+    INPUT:
+    actual - numpy array or pd series of actual y values
+    preds - numpy array or pd series of predicted y values
+    OUTPUT:
+    returns the r-squared score as a float
+    '''
+    sse = np.sum((actual-preds)**2)
+    sst = np.sum((actual-np.mean(actual))**2)
+    return 1 - sse/sst
+
+
+def mse(actual, preds):
+    '''
+    INPUT:
+    actual - numpy array or pd series of actual y values
+    preds - numpy array or pd series of predicted y values
+    OUTPUT:
+    retur)ns the mean squared error as a float
+    '''
+
+    return np.sum((actual - preds) ** 2) / len(actual)
+
+
+def mae(actual, preds):
+    '''
+    INPUT:
+    actual - numpy array or pd series of actual y values
+    preds - numpy array or pd series of predicted y values
+    OUTPUT:
+    returns the mean absolute error as a float
+    '''
+
+    return np.sum(abs(actual - preds)) / len(actual
